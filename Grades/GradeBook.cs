ec0b28e411026c0d30b74c819c8a17971a2b0f6f
Basma Ahmed <basma00ahmed@outlook.com>
2021-06-15 15:14:13 +0200

    add new class

---
+++ Grades/GradeBook.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Grades
+{
+    class GradeBook
+    {
+
+        List<float> grades;
+        public GradeBook()
+        {
+            grades = new List<float>();
+        }
+        public void AddGrade(float grade)
+        {
+            grades.Add(grade);
+        }
+    }
+}
--- Grades/Program.cs
+++ Grades/Program.cs
@@ -6,7 +6,12 @@ namespace Grades
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            GradeBook book=new GradeBook();
+            book.AddGrade(91);
+            book.AddGrade(81.5f);
+            GradeBook book2 = new GradeBook();
+            book2.AddGrade(75);
+
         }
     }
 }
